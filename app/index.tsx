32741e115b76fd0c54ceaf993d0f0328bbd07073
AmarKalel077 <AmaR.k@truneticinfotech.com>
2025-06-20 10:18:07 +0530

    android mobie

---
+++ app/index.tsx
@@ -0,0 +1,26 @@
+import { Text, View } from "react-native";
+import "../global.css";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function Index() {
+  return (
+    <SafeAreaView>
+    <View
+      // style={{
+      //   flex: 1,
+      //   justifyContent: "center",
+      //   alignItems: "center",
+      // }}
+
+
+      className=" items-center"
+
+      
+    >
+      <Text>hello</Text>
+    
+      {/* <Text className="text-lg bg-pink-400">Edit app/index.tsx to edit this screen.</Text> */}
+    </View>
+    </SafeAreaView>
+  );
+}
